031e211bfb55f1ded4c20f445079764c35d63b81
Max <maxmagi@hotmail.com>
2023-01-30 23:01:40 +0100

    Added connection to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [clothes, setClothes] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/home')
+      .then(response => {
+        console.log(response)
+        setClothes(response.data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {clothes.map((clothes: any) => (
+            <li key={clothes.id}>{clothes.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
